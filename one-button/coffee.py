1e9e2367b15fea9b34d9f304564406bb3b192eea
OneButtonDeveloper <one.button.developer@gmail.com>
2016-01-29 09:00:37 +0100

    Made possible to compile all handlebars templates into .js scripts

--- one-button/coffee.py
+++ one-button/coffee.py
@@ -1,6 +1,7 @@
 import re
 import os
 import shutil
+import collections
 from os import path
 from subprocess import call
 
@@ -22,8 +23,24 @@ def get_files_recursive(my_path, pattern):
     for (dir_path, dir_names, file_names) in os.walk(my_path):
         for file_name in file_names:
             if p.match(file_name):
-                files.append(file_name);
+                files.append(file_name)
     return files
+
+
+def delete_files(files):
+    for file_name in files:
+        if path.exists(file_name):
+            os.remove(file_name)
+
+
+def delete_files_recursive(my_path, pattern):
+    p = re.compile(pattern)
+    files = []
+    for (dir_path, dir_names, file_names) in os.walk(my_path):
+        for file_name in file_names:
+            if p.match(file_name):
+                files.append(path.join(dir_path, file_name))
+    delete_files(files)
 
 
 def find_files_recursive(my_path, pattern):
@@ -71,6 +88,27 @@ def file_ext_in(file_name, extensions):
     return file_ext(file_name) in extensions
 
 
+def concat_files(output_path, file_names, remove = True):
+    if not file_names:
+        return
+
+    with open(output_path, 'w') as outfile:
+        for file_name in file_names:
+            if path.exists(file_name):
+                with open(file_name) as infile:
+                    for line in infile:
+                        outfile.write(line)
+                outfile.write("\n")
+
+    if remove:
+        delete_files(file_names)
+
+
+def concat_files_in_directory(output_path, directory, file_names, remove = True):
+    full_file_names = [path.join(directory, file_name) for file_name in file_names]
+    concat_files(output_path, full_file_names, remove)
+
+
 def run():
     print "\n\n"
     create_build_folder()
@@ -83,10 +121,7 @@ def run():
         templates_re = ".*\.(" + ('|'.join(map(str, template_extensions))) + ")$"
 
         html_file_names = [path.splitext(f)[0] for f in get_files_recursive(BUILD_DIRECTORY_NAME, templates_re)]
-        print(html_file_names, templates_re)
-        return False
 
-        import collections
         duplicates = [item for item, count in collections.Counter(html_file_names).items() if count > 1]
 
         if duplicates:
@@ -108,6 +143,8 @@ def run():
                 shutil.move(file_name, path.join(new_path, path.basename(file_name)))
             delete_folder(html_dir)
 
+        directories_with_template_files = set()
+
         for ext in template_extensions:
             directories_with_html_files = []
             for (dir_path, dir_names, file_names) in os.walk(BUILD_DIRECTORY_NAME):
@@ -119,49 +156,34 @@ def run():
                     if file_ext(file_name) == ext:
                         directories_with_html_files.append(dir_path)
 
-            print directories_with_html_files
+            if directories_with_html_files:
+                directories_with_template_files.update(directories_with_html_files)
 
-            for directory in directories_with_html_files:
-                path_from = path.join(directory, "*." + ext)
-                new_file_name = "__handlebars." + ext + ".js"
-                path_to = path.join(directory, new_file_name)
-                if path.exists(path_to):
-                    raise NameError("Don't use " + new_file_name + " in file names. Name is reserved.")
-                print "handlebars " + path_from + " -f " + path_to + " -e " + ext
-                call("handlebars " + path_from + " -f " + path_to + " -e " + ext, shell=True)
+                for directory in directories_with_html_files:
+                    path_from = path.join(directory, "*." + ext)
+                    new_file_name = "__handlebars." + ext + ".js"
+                    path_to = path.join(directory, new_file_name)
+                    if path.exists(path_to):
+                        raise NameError("Don't use " + new_file_name + " in file names. Name is reserved.")
+                    compile_templates_command = "handlebars " + path_from + " -f " + path_to + " -e " + ext
+                    call(compile_templates_command, shell=True)
+
+        delete_files_recursive(BUILD_DIRECTORY_NAME, templates_re)
+
+        file_names_to_merge = ["__handlebars." + ext + ".js" for ext in template_extensions]
+
+        for directory_with_templates in directories_with_template_files:
+            new_path = path.join(directory_with_templates, "__handlebars.js")
+            print new_path
+            if path.exists(new_path):
+                raise NameError("Don't use __handlebars.js in file names. Name is reserved.")
+            concat_files_in_directory(new_path,  directory_with_templates, file_names_to_merge)
 
     except NameError as e:
         print("ERROR: " + e.message)
         return False
 
     return True
-
-    """
-        if os.path.basename(os.path.normpath(dir_path)) in ['html', 'handlebars']:
-            file_name, file_extension = os.path.splitext('/path/to/somefile.ext')
-            print dir_path
-
-    system_files = [file_name for file_name in get_files_in_folder("main/coffee/content", "^_.*coffee$")]
-    files = [file_name for file_name in get_files_in_folder("main/coffee/content", "^[^_].*coffee$")]
-    files.sort()
-    files = system_files + files
-
-    for file_name in files:
-        lines_to_analyze = []
-        f = open(file_name, 'r')
-        line = f.readline()
-        print("first line: " + line)
-        while line is not None and line.startswith("#"):
-            lines_to_analyze.append(line)
-            line = f.readline()
-        f.close()
-
-        print file_name
-        for line in lines_to_analyze:
-            print line
-
-    print "This line will be printed."
-    """
 
 
 run()
